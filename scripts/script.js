3354a01ad53100273f0674dcf33caec2da84d0ef
kazonhavel1 <joaopedroescola63@gmail.com>
2024-06-13 21:30:08 -0300

    Ajustes da Base do Site

--- scripts/script.js
+++ scripts/script.js
@@ -11,3 +11,17 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+function mostrarAlerta() {
+    Swal.fire({
+        title: 'Ótima Escolha!',
+        text: 'Chame uma das Garçonetes e faça seu pedido.',
+        icon: 'info',
+        confirmButtonText: 'OK',
+        timer: 5000,
+        timerProgressBar: true,
+        backdrop: 
+                'rgba(141,73,37,0.2)',
+        confirmButtonColor: '#78350f',
+    });
+}
